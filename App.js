980758d53ebd79dca84263f0355087b2cd03fda6
Areum <combi_areum@naver.com>
2021-07-15 13:22:32 +0900

    getLocation and getTemperature

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {Alert} from "react-native";
-import Loading from "./Loading";
-import * as Location from 'expo-location';
+import React from "react";
+import { Alert } from "react-native";
+import Lodaing from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+import api from "./secrets.json";
 
-const API_KEY = "";
+const API_KEY = api.API_KEY;
 
-export default class extends React.Component{
+export default class extends React.Component {
   state = {
-    isLoading: true
-  }
-  getLocation = async() => {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
+  getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync();
-
-      const {coords: {latitude, longitude} } = await Location.getCurrentPositionAsync();
-      //this.setState( {isLoading: false} );
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
-      //만약 사용자가 permission을 주지 않는다면 어떻게 할지에 대한 정보
-      Alert.alert("Can't find you.", "Where are you?");
+      // 만약 사용자가 permission을 주지 않는다면 어떻게 할 지에 대한 정보
+      Alert.alert("Can't find you! Where are you?");
     }
-    
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     this.getLocation();
   }
-  render(){
-    const {isLoading} = this.state
-    return isLoading ? <Loading /> : null;
+  render() {
+    const { isLoading, temp } = this.state;
+    //
+    return isLoading ? <Lodaing /> : <Weather temp={Math.round(temp)} />;
   }
-  
 }
+>>>>>>> c5c9006 (getLocation and getTemperature)
